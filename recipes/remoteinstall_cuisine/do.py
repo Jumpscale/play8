fb0d58db2258ff9cca15468448f2e0a50af5fddc
kristof de spiegeleer <kristof@incubaid.com>
2016-03-11 07:18:05 +0400

    new

--- recipes/remoteinstall_cuisine/do.py
+++ recipes/remoteinstall_cuisine/do.py
@@ -40,34 +40,57 @@ def preparehost(host="", keyname="",pushkey=False):
 @click.command()
 @click.option('--host', '-h', help='connectionstring e.g. myserver:2022')
 @click.option('--keyname', '-k', help='nameof key to use, if not given will ask', default="")
-def buildall(host="", keyname=""):
+def buildallInDocker(host="", keyname=""):
     """
     example:
     python3 do.py buildall --host '192.168.0.250:22' --keyname 'ovh_install'
     """
+    def dockerbuild(host):
+        c=j.tools.cuisine.get(host)
 
-    c=j.tools.cuisine.get(host)
+        #build ubuntu from docker file
+        print ("BUILD UBUNTU WITH SSH SUPPORT")
+        c.docker.ubuntuBuild()
 
-    #build ubuntu from docker file
-    print ("BUILD UBUNTU WITH SSH SUPPORT")
-    c.docker.ubuntuBuild()
+    buildaction=j.actions.add(dockerbuild,args=[host],stdOutput=True,retry=2,executeNow=False)
 
-    pubkey=j.clients.ssh.getSSHKeyFromAgentPub(keyname)
+    
+    def dockerstart(keyname,host):
+        c=j.tools.cuisine.get(host)
+        pubkey=j.clients.ssh.getSSHKeyFromAgentPub(keyname)
+        print ("LAUNCH UBUNTU DOCKER WITH MY PUB LOCAL SSH KEY")
+        connstr=c.docker.ubuntu(name="ubuntu_build", image='jumpscale/ubuntu1510', ports=None, volumes=None, pubkey=pubkey, aydofs=False)
+        return connstr
 
 
-    print ("LAUNCH UBUNTU DOCKER WITH MY PUB LOCAL SSH KEY")
-    connstr=c.docker.ubuntu(name="ubuntu_build", image='jumpscale/ubuntu1510', ports=None, volumes=None, pubkey=pubkey, aydofs=False)
+    dockerstart=j.actions.add(dockerbuild,args=[keyname,host],stdOutput=True,retry=2,executeNow=False,deps=[buildaction])
 
-    connstr="ovh4:9025"
-    print ("connectionstring of docker:%s"%connstr)
+    connstr=dockerstart.result
 
-    c2=j.tools.cuisine.get(connstr)
-    c2.builder.all()
+    def dockerbuildjs8(connstr):
+        print ("DOCKERBUILD JS8")
+        print ("connectionstring of docker:%s"%connstr)
+        c2=j.tools.cuisine.get(connstr)
+        c2.builder.all()
+
+    dockerbuildjs8=j.actions.add(dockerbuildjs8,args=[connstr],stdOutput=True,retry=2,executeNow=False,deps=[dockerstart])
+
+    def pushdocker(host):
+        c=j.tools.cuisine.get(host)
+        from IPython import embed
+        print ("DEBUG NOW pushdocker")
+        embed()
+        
+
+    dockerbuildjs8=j.actions.add(pushdocker,args=[host],stdOutput=True,retry=2,executeNow=False,deps=[dockerbuildjs8])
+
+    j.actions.run()
+
 
 
 cli.add_command(pushsshkey)
 cli.add_command(preparehost)
-cli.add_command(buildall)
+cli.add_command(buildallInDocker)
 
 if __name__ == '__main__':
     cli()
