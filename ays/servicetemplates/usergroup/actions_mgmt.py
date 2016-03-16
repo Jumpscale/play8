257eda0a38facaf35953f3c86cfe576f9cc8b146
root <root@js8>
2016-03-16 05:12:54 +0000

    ays changed, commit changed files before deploy of blueprints

--- ays/servicetemplates/usergroup/actions_mgmt.py
+++ ays/servicetemplates/usergroup/actions_mgmt.py
@@ -5,8 +5,8 @@ from JumpScale import j
 ActionsBase=j.atyourservice.getActionsBaseClassMgmt()
 
 class Actions(ActionsBase):
-    
-    def input(self,serviceObj):
+
+    def input(self):
         #need to find the members based on tel or email addr
 
         #prob not needed
@@ -14,9 +14,9 @@ class Actions(ActionsBase):
 
         membersfound=[]
 
-        if "users" in serviceObj.args:            
-            serviceObj.args["users"]=j.data.text.getList(serviceObj.args["users"])
-            for email_tel_name in serviceObj.args["users"]:
+        if "users" in self.service.args:
+            self.service.args["users"]=j.data.text.getList(self.service.args["users"])
+            for email_tel_name in self.service.args["users"]:
                 #find the member
                 found=False
                 for user in j.atyourservice.findServices(role="user"):
@@ -29,36 +29,36 @@ class Actions(ActionsBase):
                         found=True
                         continue
                 if found==False:
-                    msg="could not find user:\n%s\ntrying to create group:\n%s"%(user,serviceObj)
+                    msg="could not find user:\n%s\ntrying to create group:\n%s"%(user,self.service)
                     raise RuntimeError(msg)
 
-        if "groups" in serviceObj.args:
-            serviceObj.args["groups"]=j.data.text.getList(serviceObj.args["groups"])
-            for groupname in serviceObj.args["groups"]:
+        if "groups" in self.service.args:
+            self.service.args["groups"]=j.data.text.getList(self.service.args["groups"])
+            for groupname in self.service.args["groups"]:
                 #find the group
                 groups= j.atyourservice.findServices(role="usergroup",instance=groupname)
                 if len(groups)==0:
-                    msg="could not find group:\n%s\ntrying to create group:\n%s"%(groupname,serviceObj)
+                    msg="could not find group:\n%s\ntrying to create group:\n%s"%(groupname,self.service)
                     raise RuntimeError(msg)
                 elif len(groups)>1:
-                    msg="found more than 1 group:\n%s\ntrying to create group:\n%s"%(groupname,serviceObj)
+                    msg="found more than 1 group:\n%s\ntrying to create group:\n%s"%(groupname,self.service)
                     raise RuntimeError(msg)
                 else:
                     group=groups[0]
-                    for member in group.hrd.getList("members"): 
+                    for member in group.hrd.getList("members"):
                         if member not in membersfound:
                             membersfound.append(member)
 
         #this gives us the instance names, independant from which tel or email address was used for identification
-        serviceObj.args["members"]=membersfound
+        self.service.args["members"]=membersfound
 
-        ActionsBase.input(self,serviceObj)
-        
+        ActionsBase.input(self,self.service)
 
-    def install_pre(self, service_obj):
+
+    def install_pre(self):
 
         print("check with id mgmt system if user exists")
 
-    def install_post(self, service_obj):
+    def install_post(self):
 
         print("send email to customer to let him know he exists")
